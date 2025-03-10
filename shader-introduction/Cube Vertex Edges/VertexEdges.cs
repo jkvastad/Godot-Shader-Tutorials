90705974a48743a5c0e4311d39d18774c7ffeb89
jkvastad <jkvastad@gmail.com>
2025-03-10 11:50:43 +0100

    Added tweeners to show distinction versus vertex based edges and fragment based edges

---
+++ shader-introduction/Cube Vertex Edges/VertexEdges.cs
@@ -0,0 +1,20 @@
+using Godot;
+
+public partial class VertexEdges : Node
+{
+    [Export]
+    MeshInstance3D box2;
+    public override void _Ready()
+    {
+        Tween boxMotion = GetTree().CreateTween();
+        boxMotion.SetTrans(Tween.TransitionType.Sine).SetLoops();
+        boxMotion.TweenInterval(1);
+        boxMotion.TweenProperty(box2, "position:x", 1, 2.0);
+        boxMotion.TweenInterval(1);
+        boxMotion.TweenProperty(box2, "position:x", 0, 2.0);
+        boxMotion.TweenInterval(1);
+        boxMotion.TweenProperty(box2, "position:x", 1, 2.0);
+        boxMotion.TweenInterval(1);
+        boxMotion.TweenProperty(box2, "position:x", 2, 2.0);        
+    }
+}
